1e3c92b3e87b381aaea1d718b9f253157de88261
alberto <albertodelviejosanchez@gmail.com>
2020-12-16 19:40:21 +0100

    Last update

--- lib/ui/widgets/rounded_button.dart
+++ lib/ui/widgets/rounded_button.dart
@@ -25,7 +25,7 @@ class RoundedButton extends StatelessWidget {
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10.0),
             gradient: LinearGradient(
-              List: [backgroundColor, backgroundColor],
+              colors: [backgroundColor, backgroundColor],
               begin: FractionalOffset(0.2, 0.0),
               end: FractionalOffset(1.0, 0.6),
             )),
